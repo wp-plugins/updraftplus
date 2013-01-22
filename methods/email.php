9ff43cc011e0350d099114622ce34a9080d35918
DavidAnderson <DavidAnderson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-22 19:39:29 +0000

    1.2.46 - detect overlaps better
    git-svn-id: https://plugins.svn.wordpress.org/updraftplus/tags/1.2.46@657048 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- methods/email.php
+++ methods/email.php
@@ -13,12 +13,16 @@ class UpdraftPlus_BackupModule_email {
 			wp_mail(get_option('updraft_email'), "WordPress Backup ".date('Y-m-d H:i',$updraftplus->backup_time), "Backup is of the $type.  Be wary; email backups may fail because of file size limitations on mail servers.", null, array($fullpath));
 			$updraftplus->uploaded_file($file);
 		}
-
-		$updraftplus->prune_retained_backups("local", null, null);
+		$updraftplus->prune_retained_backups("email", null, null);
 	}
 
 	function config_print() {
-		return false;
+		?>
+		<tr class="updraftplusmethod email">
+			<th>Note:</th>
+			<td>The email address entered above will be used. If choosing &quot;E-Mail&quot;, then be aware that mail servers tend to have size limits; typically around 10-20Mb; backups larger than any limits will not arrive. If you really need a large backup via email, then you could fund a new feature (to break the backup set into configurable-size pieces) - but the demand has not yet existed for such a feature.</td>
+		</tr>
+		<?php
 	}
 
 }
