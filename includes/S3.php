514a1a22599d21259f314101471515f193b6ae1d
DavidAnderson <DavidAnderson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-30 14:39:37 +0000

    1.3.20 - S3 logging loop
    git-svn-id: https://plugins.svn.wordpress.org/updraftplus/tags/1.3.20@661260 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/S3.php
+++ includes/S3.php
@@ -535,18 +535,18 @@ class S3
 		$rest->setHeader('Content-Type', 'application/octet-stream');
 		$rest->data = "";
 
-		$handle = fopen($filePath, "rb");
-		if ($fileOffset >0) fseek($handle, $fileOffset);
-		$bytes_read = 0;
-
-		while ($fileBytes >0) {
-			$read = fread($handle, min($fileBytes, 32768));
-			$fileBytes = $fileBytes - strlen($read);
-			$bytes_read += strlen($read);
-			$rest->data = $rest->data . $read;
+		if ($handle = fopen($filePath, "rb")) {
+			if ($fileOffset >0) fseek($handle, $fileOffset);
+			$bytes_read = 0;
+			while ($fileBytes>0 && $read = fread($handle, max($fileBytes, 65536))) {
+				$fileBytes = $fileBytes - strlen($read);
+				$bytes_read += strlen($read);
+				$rest->data = $rest->data . $read;
+			}
+			fclose($handle);
+		} else {
+			return false;
 		}
-
-		fclose($handle);
 
  		$rest->setHeader('Content-MD5', base64_encode(md5($rest->data, true)));
 		$rest->size = $bytes_read;
