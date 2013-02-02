f001f6d3b5ec60b978e7e5da328458ed9279ec35
DavidAnderson <DavidAnderson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-02 18:36:10 +0000

    1.3.24 - different zip recursion method, allowing for resumption later + more PHP compatibility
    git-svn-id: https://plugins.svn.wordpress.org/updraftplus/tags/1.3.24@662527 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- methods/email.php
+++ methods/email.php
@@ -9,16 +9,20 @@ class UpdraftPlus_BackupModule_email {
 		global $updraftplus;
 
 		foreach ($backup_array as $type => $file) {
-			$fullpath = trailingslashit(get_option('updraft_dir')).$file;
-			wp_mail(get_option('updraft_email'), "WordPress Backup ".date('Y-m-d H:i',$updraftplus->backup_time), "Backup is of the $type.  Be wary; email backups may fail because of file size limitations on mail servers.", null, array($fullpath));
+			$fullpath = trailingslashit(UpdraftPlus_Options::get_updraft_option('updraft_dir')).$file;
+			wp_mail(UpdraftPlus_Options::get_updraft_option('updraft_email'), "WordPress Backup ".date('Y-m-d H:i',$updraftplus->backup_time), "Backup is of the $type.  Be wary; email backups may fail because of file size limitations on mail servers.", null, array($fullpath));
 			$updraftplus->uploaded_file($file);
 		}
-
-		$updraftplus->prune_retained_backups("local", null, null);
+		$updraftplus->prune_retained_backups("email", null, null);
 	}
 
-	function config_print() {
-		return false;
+	public static function config_print() {
+		?>
+		<tr class="updraftplusmethod email">
+			<th>Note:</th>
+			<td>The email address entered above will be used. If choosing &quot;E-Mail&quot;, then be aware that mail servers tend to have size limits; typically around 10-20Mb; backups larger than any limits will not arrive. If you really need a large backup via email, then you could fund a new feature (to break the backup set into configurable-size pieces) - but the demand has not yet existed for such a feature.</td>
+		</tr>
+		<?php
 	}
 
 }
--- methods/ftp.php
+++ methods/ftp.php
@@ -8,22 +8,33 @@ class UpdraftPlus_BackupModule_ftp {
 
 		if( !class_exists('ftp_wrapper')) require_once(UPDRAFTPLUS_DIR.'/includes/ftp.class.php');
 
-		//handle SSL and errors at some point TODO
-		$ftp = new ftp_wrapper(get_option('updraft_server_address'), get_option('updraft_ftp_login'), get_option('updraft_ftp_pass'));
+		$server = UpdraftPlus_Options::get_updraft_option('updraft_server_address');
+		$ftp = new ftp_wrapper($server , UpdraftPlus_Options::get_updraft_option('updraft_ftp_login'), UpdraftPlus_Options::get_updraft_option('updraft_ftp_pass'));
 		$ftp->passive = true;
-		$ftp->connect();
+
+		if (!$ftp->connect()) {
+			$ftp->ssl = true;
+			if (!$ftp->connect()) {
+				$updraftplus->log("FTP Failure: we did not successfully log in with those credentials.");
+				$updraftplus->error("FTP login failure");
+				return false;
+			}
+		}
+
 		//$ftp->make_dir(); we may need to recursively create dirs? TODO
 		
-		$ftp_remote_path = trailingslashit(get_option('updraft_ftp_remote_path'));
+		$ftp_remote_path = trailingslashit(UpdraftPlus_Options::get_updraft_option('updraft_ftp_remote_path'));
 		foreach($backup_array as $file) {
-			$fullpath = trailingslashit(get_option('updraft_dir')).$file;
-			$updraftplus->log("FTP upload attempt: $file -> $ftp_remote_path/$file");
-			if ($ftp->put($fullpath,$ftp_remote_path.$file,FTP_BINARY)) {
-				$updraftplus->log("ERROR: $file_name: Successfully uploaded via FTP");
+			$fullpath = trailingslashit(UpdraftPlus_Options::get_updraft_option('updraft_dir')).$file;
+			$updraftplus->log("FTP upload attempt: $file -> ftp://$server/${ftp_remote_path}${file}");
+			$timer_start = microtime(true);
+			$size_k = round(filesize($fullpath)/1024,1);
+			if ($ftp->put($fullpath, $ftp_remote_path.$file, FTP_BINARY)) {
+				$updraftplus->log("FTP upload attempt successful (".$size_k."Kb in ".(round(microtime(true)-$timer_start,2)).'s)');
 				$updraftplus->uploaded_file($file);
 			} else {
-				$updraftplus->error("$file_name: Failed to upload to FTP" );
-				$updraftplus->log("ERROR: $file_name: Failed to upload to FTP" );
+				$updraftplus->log("ERROR: FTP upload failed" );
+				$updraftplus->error("FTP upload failed" );
 			}
 		}
 
@@ -31,49 +42,132 @@ class UpdraftPlus_BackupModule_ftp {
 	}
 
 	function delete($file, $ftparr) {
+		global $updraftplus;
 		$ftp = $ftparr['ftp_object'];
 		$ftp_remote_path = $ftparr['ftp_remote_path'];
-		@$ftp->delete($ftp_remote_path.$file);
+		if (@$ftp->delete($ftp_remote_path.$file)) {
+			$updraftplus->log("FTP delete: succeeded (${ftp_remote_path}${file})");
+		} else {
+			$updraftplus->log("FTP delete: failed (${ftp_remote_path}${file})");
+		}
 	}
 
 	function download($file) {
 		if( !class_exists('ftp_wrapper')) require_once(UPDRAFTPLUS_DIR.'/includes/ftp.class.php');
 
-		//handle SSL and errors at some point TODO
-		$ftp = new ftp_wrapper(get_option('updraft_server_address'),get_option('updraft_ftp_login'),get_option('updraft_ftp_pass'));
+		//handle errors at some point TODO
+		$ftp = new ftp_wrapper(UpdraftPlus_Options::get_updraft_option('updraft_server_address'),UpdraftPlus_Options::get_updraft_option('updraft_ftp_login'),UpdraftPlus_Options::get_updraft_option('updraft_ftp_pass'));
 		$ftp->passive = true;
-		$ftp->connect();
+
+		if (!$ftp->connect()) {
+			$ftp->ssl = true;
+			if (!$ftp->connect()) {
+				$updraftplus->log("FTP Failure: we did not successfully log in with those credentials.");
+				$updraftplus->error("FTP login failure");
+				return false;
+			}
+		}
+
 		//$ftp->make_dir(); we may need to recursively create dirs? TODO
 		
-		$ftp_remote_path = trailingslashit(get_option('updraft_ftp_remote_path'));
-		$fullpath = trailingslashit(get_option('updraft_dir')).$file;
-		$ftp->get($fullpath,$ftp_remote_path.$file,FTP_BINARY);
+		$ftp_remote_path = trailingslashit(UpdraftPlus_Options::get_updraft_option('updraft_ftp_remote_path'));
+		$fullpath = trailingslashit(UpdraftPlus_Options::get_updraft_option('updraft_dir')).$file;
+		$ftp->get($fullpath, $ftp_remote_path.$file, FTP_BINARY);
 	}
 
-	function config_print() {
+	public static function config_print_javascript_onready() {
+		?>
+		jQuery('#updraft-ftp-test').click(function(){
+			var data = {
+				action: 'updraft_ajax',
+				subaction: 'credentials_test',
+				method: 'ftp',
+				nonce: '<?php echo wp_create_nonce('updraftplus-credentialtest-nonce'); ?>',
+				server: jQuery('#updraft_server_address').val(),
+				login: jQuery('#updraft_ftp_login').val(),
+				pass: jQuery('#updraft_ftp_pass').val(),
+				path: jQuery('#updraft_ftp_remote_path').val()
+			};
+			jQuery.post(ajaxurl, data, function(response) {
+					alert('Settings test result: ' + response);
+			});
+		});
+		<?php
+	}
+
+	public static function config_print() {
 		?>
 		<tr class="updraftplusmethod ftp">
 			<th>FTP Server:</th>
-			<td><input type="text" style="width:260px" name="updraft_server_address" value="<?php echo htmlspecialchars(get_option('updraft_server_address')); ?>" /></td>
+			<td><input type="text" size="40" id="updraft_server_address" name="updraft_server_address" value="<?php echo htmlspecialchars(UpdraftPlus_Options::get_updraft_option('updraft_server_address')); ?>" /> <em>Both SSL and non-SSL are supported</em></td>
 		</tr>
 		<tr class="updraftplusmethod ftp">
 			<th>FTP Login:</th>
-			<td><input type="text" autocomplete="off" name="updraft_ftp_login" value="<?php echo htmlspecialchars(get_option('updraft_ftp_login')) ?>" /></td>
+			<td><input type="text" size="40" id="updraft_ftp_login" name="updraft_ftp_login" value="<?php echo htmlspecialchars(UpdraftPlus_Options::get_updraft_option('updraft_ftp_login')) ?>" /></td>
 		</tr>
 		<tr class="updraftplusmethod ftp">
 			<th>FTP Password:</th>
-			<td><input type="text" autocomplete="off" style="width:260px" name="updraft_ftp_pass" value="<?php echo htmlspecialchars(get_option('updraft_ftp_pass')); ?>" /></td>
+			<td><input type="text" size="40" id="updraft_ftp_pass" name="updraft_ftp_pass" value="<?php echo htmlspecialchars(UpdraftPlus_Options::get_updraft_option('updraft_ftp_pass')); ?>" /></td>
 		</tr>
 		<tr class="updraftplusmethod ftp">
 			<th>Remote Path:</th>
-			<td><input type="text" style="width:260px" name="updraft_ftp_remote_path" value="<?php echo htmlspecialchars(get_option('updraft_ftp_remote_path')); ?>" /></td>
+			<td><input type="text" size="64" id="updraft_ftp_remote_path" name="updraft_ftp_remote_path" value="<?php echo htmlspecialchars(UpdraftPlus_Options::get_updraft_option('updraft_ftp_remote_path')); ?>" /> <em>Needs to already exist</em></td>
+		</tr>
+		<tr class="updraftplusmethod ftp">
+		<th></th>
+		<td><p><button id="updraft-ftp-test" type="button" class="button-primary" style="font-size:18px !important">Test FTP Login</button></p></td>
 		</tr>
 		<?php
 	}
 
-	function file_delete() {
-		$this->log("$backup_datestamp: Delete remote ftp: $remote_path/$dofile");
-		@$remote_object->delete($remote_path.$dofile);
+	public static function credentials_test() {
+
+		$server = $_POST['server'];
+		$login = $_POST['login'];
+		$pass = $_POST['pass'];
+		$path = $_POST['path'];
+
+		if (empty($server)) {
+			echo "Failure: No server details were given.";
+			return;
+		}
+		if (empty($login)) {
+			echo "Failure: No login was given.";
+			return;
+		}
+		if (empty($pass)) {
+			echo "Failure: No password was given.";
+			return;
+		}
+
+		if( !class_exists('ftp_wrapper')) require_once(UPDRAFTPLUS_DIR.'/includes/ftp.class.php');
+
+		//handle SSL and errors at some point TODO
+		$ftp = new ftp_wrapper($server, $login, $pass);
+		$ftp->passive = true;
+
+		if (!$ftp->connect()) {
+			$ftp->ssl = true;
+			if (!$ftp->connect()) {
+				echo "Failure: we did not successfully log in with those credentials.";
+				return;
+			}
+		}
+		//$ftp->make_dir(); we may need to recursively create dirs? TODO
+
+		$file = md5(rand(0,99999999)).'.tmp';
+		$fullpath = trailingslashit($path).$file;
+		if (!file_exists(ABSPATH.'wp-includes/version.php')) {
+			echo "Failure: an unexpected internal UpdraftPlus error occurred when testing the credentials - please contact the developer";
+			return;
+		}
+		if ($ftp->put(ABSPATH.'wp-includes/version.php', $fullpath, FTP_BINARY)) {
+			echo "Success: we successfully logged in, and confirmed our ability to create a file in the given directory.";
+			@$ftp->delete($fullpath);
+		} else {
+			echo "Failure: we successfully logged in, but were not able to create a file in the given directory.";
+		}
+
 	}
 
 }
