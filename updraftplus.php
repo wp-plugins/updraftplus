1bcadc33a2ea7764634d96773171060b8544cfd2
DavidAnderson <DavidAnderson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-10 20:51:44 +0000

    1.5.9 - clearstatcache was being called wrongly
    git-svn-id: https://plugins.svn.wordpress.org/updraftplus/tags/1.5.9@695478 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- updraftplus.php
+++ updraftplus.php
@@ -368,6 +368,24 @@ class UpdraftPlus {
 		$this->current_resumption = $resumption_no;
 		$this->log("Backup run: resumption=$resumption_no, nonce=$bnonce, begun at=$btime (${time_ago}s ago), job type: $job_type");
 
+		if ($resumption_no == 8) {
+			$time_passed = $this->jobdata_get('run_times');
+			if (!is_array($time_passed)) $time_passed = array();
+			$timings_string = "";
+			$run_times_known=0;
+			for ($i=0; $i<=7; $i++) {
+				$timings_string .= "$i:";
+				if (isset($time_passed[$i])) {
+					$timings_string .=  round($time_passed[$i], 1).' ';
+					$run_times_known++;
+				} else {
+					$timings_string .=  '? ';
+				}
+			}
+			$this->log("Time passed on previous resumptions: $passed");
+			// TODO: If there's sufficient data and an upper limit clearly lower than our present resume_interval, then decrease the resume_interval
+		}
+
 		// Schedule again, to run in 5 minutes again, in case we again fail
 		// The actual interval can be increased (for future resumptions) by other code, if it detects apparent overlapping
 		$resume_interval = $this->jobdata_get('resume_interval');
@@ -381,7 +399,7 @@ class UpdraftPlus {
 			wp_schedule_single_event($schedule_for, 'updraft_backup_resume', array($next_resumption, $bnonce));
 			$this->newresumption_scheduled = $schedule_for;
 		} else {
-			$this->log(sprintf('The current run is attempt number %d - will not schedule a further attempt until we see something useful happening'), 10);
+			$this->log(sprintf('The current run is attempt number %d - will not schedule a further attempt until we see something useful happening', 10));
 		}
 
 		// Sanity check
@@ -3088,7 +3106,7 @@ class UpdraftPlus {
 		if (file_exists($zipfile)) {
 			$opencode = $zip->open($zipfile);
 			$original_size = filesize($zipfile);
-			clearstatcache($zipfile);
+			clearstatcache();
 		} else {
 			$opencode = $zip->open($zipfile, ZIPARCHIVE::CREATE);
 			$original_size = 0;
@@ -3111,7 +3129,7 @@ class UpdraftPlus {
 				if ($data_added_since_reopen > 26214400 || (time() - $this->zipfiles_lastwritetime) > 2) {
 
 					$before_size = filesize($zipfile);
-					clearstatcache($zipfile);
+					clearstatcache();
 
 					if ($data_added_since_reopen > 26214400) {
 						$this->log("Adding batch to zip file: over 25Mb added on this batch (".round($data_added_since_reopen/1048576,1)." Mb); re-opening (prior size: ".round($before_size/1024,1).' Kb)');
@@ -3129,7 +3147,7 @@ class UpdraftPlus {
 					$this->zipfiles_lastwritetime = time();
 					// Call here, in case we've got so many big files that we don't complete the whole routine
 					if (filesize($zipfile) > $before_size) $this->something_useful_happened();
-					clearstatcache($zipfile);
+					clearstatcache();
 				}
 			}
 			$this->zipfiles_added++;
@@ -3141,11 +3159,18 @@ class UpdraftPlus {
 		$ret =  $zip->close();
 		$this->zipfiles_lastwritetime = time();
 		if (filesize($zipfile) > $original_size) $this->something_useful_happened();
-		clearstatcache($zipfile);
+		clearstatcache();
 		return $ret;
 	}
 
 	function something_useful_happened() {
+
+		// First, update the record of maximum detected runtime on each run
+		$time_passed = $this->jobdata_get('run_times');
+		if (!is_array($time_passed)) $time_passed = array();
+		$time_passed[$this->current_resumption] = microtime(true)-$this->opened_log_time;
+		$this->jobdata_set('run_times', $time_passed);
+
 		if ($this->current_resumption >= 9 && $this->newresumption_scheduled == false) {
 			$resume_interval = $this->jobdata_get('resume_interval');
 			if (!is_numeric($resume_interval) || $resume_interval<$this->minimum_resume_interval()) { $resume_interval = $this->minimum_resume_interval(); }
