864b942b7a55e8902efd53d0dea3a67a6e99323c
DavidAnderson <DavidAnderson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-04 07:11:14 +0000

    replace a method_exists with class_exists to avoid segfault
    git-svn-id: https://plugins.svn.wordpress.org/updraftplus/tags/1.6.17@735859 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- updraftplus.php
+++ updraftplus.php
@@ -1055,7 +1055,7 @@ class UpdraftPlus {
 		}
 
 		$objname = "UpdraftPlus_BackupModule_${service}";
-		if (method_exists($objname, "backup")) {
+		if (class_exists($objname)) {
 			// New style - external, allowing more plugability
 			$remote_obj = new $objname;
 			$remote_obj->backup($backup_array);
