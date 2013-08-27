c9be86693c030ac77b9837d56da7c0219e0dca09
DavidAnderson <DavidAnderson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-27 14:53:07 +0000

    reduce db filename max chars to be consistent with zips
    git-svn-id: https://plugins.svn.wordpress.org/updraftplus/tags/1.7.3@763082 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- backup.php
+++ backup.php
@@ -615,7 +615,7 @@ class UpdraftPlus_Backup {
 		if (!$updraftplus->opened_log_time) $updraftplus->logfile_open($updraftplus->nonce);
 
 		// Get the blog name and rip out all non-alphanumeric chars other than _
-		$blog_name = preg_replace('/[^A-Za-z0-9_]/','', str_replace(' ','_', substr(get_bloginfo(), 0, 96)));
+		$blog_name = preg_replace('/[^A-Za-z0-9_]/','', str_replace(' ','_', substr(get_bloginfo(), 0, 32)));
 		if (!$blog_name) $blog_name = 'non_alpha_name';
 		$blog_name = apply_filters('updraftplus_blog_name', $blog_name);
 
