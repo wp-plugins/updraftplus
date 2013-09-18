1eb0562f2fbf2b6b5a2af7525e5a2b3879ecce87
DavidAnderson <DavidAnderson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-18 10:30:21 +0000

    timing tweak
    git-svn-id: https://plugins.svn.wordpress.org/updraftplus/tags/1.7.18@774520 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/updraft-admin-ui.js
+++ includes/updraft-admin-ui.js
@@ -428,14 +428,14 @@ jQuery(document).ready(function($){
 			setTimeout(function() {jQuery.get(updraft_siteurl);}, 5100);
 			// If the site has cron or loopbacks disabled, then there may be a backlog to clear... if the site does not have cron disabled, then these have no performance impact
 			setTimeout(function() {jQuery.get(updraft_siteurl+'/wp-cron.php', function() {
-				setTimeout(function() {jQuery.get(updraft_siteurl+'/wp-cron.php');}, 6000);
-			} );}, 5400);
+				setTimeout(function() {jQuery.get(updraft_siteurl+'/wp-cron.php');}, 8000);
+			} );}, 6400);
 			setTimeout(function() {jQuery.get(updraft_siteurl+'/wp-cron.php', function() {
-				setTimeout(function() {jQuery.get(updraft_siteurl+'/wp-cron.php');}, 6000);
-			} );}, 17400);
+				setTimeout(function() {jQuery.get(updraft_siteurl+'/wp-cron.php');}, 8000);
+			} );}, 18400);
 			setTimeout(function() {jQuery.get(updraft_siteurl+'/wp-cron.php', function() {
-				setTimeout(function() {jQuery.get(updraft_siteurl+'/wp-cron.php');}, 6000);
-			} );}, 27400);
+				setTimeout(function() {jQuery.get(updraft_siteurl+'/wp-cron.php');}, 8000);
+			} );}, 30400);
 			
 			//setTimeout(function() {updraft_showlastlog();}, 6000);
 			setTimeout(function() {updraft_activejobs_update();}, 6000);
