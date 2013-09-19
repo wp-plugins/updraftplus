0da4a3914c418a65f1f44bac65adedb3852735f8
DavidAnderson <DavidAnderson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-19 08:06:53 +0000

    fix updraft_dir setting for corner-case where someone installs after de-installing + deleting dir
    git-svn-id: https://plugins.svn.wordpress.org/updraftplus/tags/1.7.18@775151 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/updraft-admin-ui.js
+++ includes/updraft-admin-ui.js
@@ -428,15 +428,12 @@ jQuery(document).ready(function($){
 			setTimeout(function() {jQuery.get(updraft_siteurl);}, 5100);
 			// If the site has cron or loopbacks disabled, then there may be a backlog to clear... if the site does not have cron disabled, then these have no performance impact
 			setTimeout(function() {jQuery.get(updraft_siteurl+'/wp-cron.php', function() {
-				setTimeout(function() {jQuery.get(updraft_siteurl+'/wp-cron.php');}, 8000);
-			} );}, 6400);
-			setTimeout(function() {jQuery.get(updraft_siteurl+'/wp-cron.php', function() {
-				setTimeout(function() {jQuery.get(updraft_siteurl+'/wp-cron.php');}, 8000);
-			} );}, 18400);
-			setTimeout(function() {jQuery.get(updraft_siteurl+'/wp-cron.php', function() {
-				setTimeout(function() {jQuery.get(updraft_siteurl+'/wp-cron.php');}, 8000);
-			} );}, 30400);
-			
+				setTimeout(function() { jQuery.get(updraft_siteurl+'/wp-cron.php', function() {
+					setTimeout(function() { jQuery.get(updraft_siteurl+'/wp-cron.php', function() {
+						setTimeout(function() { jQuery.get(updraft_siteurl+'/wp-cron.php'); }, 10000);
+					}); }, 10000);
+				});}, 10000);
+			} );}, 5600);
 			//setTimeout(function() {updraft_showlastlog();}, 6000);
 			setTimeout(function() {updraft_activejobs_update();}, 6000);
 			setTimeout(function() {
