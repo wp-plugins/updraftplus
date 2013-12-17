a0d7fa85b5e7d6c6556dc2f3257236de1de30c3f
DavidAnderson <DavidAnderson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-17 11:25:33 +0000

    move rackspace SDK - shorten path lengths
    git-svn-id: https://plugins.svn.wordpress.org/updraftplus/trunk@823801 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- oc/composer/autoload_namespaces.php
+++ oc/composer/autoload_namespaces.php
@@ -7,7 +7,7 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Symfony\\Component\\EventDispatcher\\' => array($vendorDir . '/symed'),
-    'OpenCloud' => array($vendorDir . '/rs/oc/lib'),
+    'OpenCloud' => array($vendorDir . '/rackspace/php-opencloud/lib', $vendorDir . '/rackspace/php-opencloud/tests'),
     'Guzzle\\Stream' => array($vendorDir . '/guzzle/stream'),
     'Guzzle\\Parser' => array($vendorDir . '/guzzle/parser'),
     'Guzzle\\Http' => array($vendorDir . '/guzzle/http'),
